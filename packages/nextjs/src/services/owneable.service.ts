d279cc6a04b7cdf817a45960924af7fdbe48b43c
marian2js <marianomdq1@gmail.com>
2024-06-08 14:07:32 +0200

    feat: allow to customize fetch options on owneable service methods

--- packages/nextjs/src/services/owneable.service.ts
+++ packages/nextjs/src/services/owneable.service.ts
@@ -2,41 +2,55 @@
 
 import { fetchWithAuth } from '../utils/fetch.utils'
 
-export async function fetchOwneableItems<T>(entityKey: string): Promise<T[]> {
-  const res = await fetchWithAuth<{ items: T[] }>(`/${entityKey}`)
+export async function fetchOwneableItems<T>(entityKey: string, options?: RequestInit): Promise<T[]> {
+  const res = await fetchWithAuth<{ items: T[] }>(`/${entityKey}`, options)
   return res.items
 }
 
-export async function fetchOwneableItem<T>(entityKey: string, itemId: string): Promise<T> {
-  const res = await fetchWithAuth<{ item: T }>(`/${entityKey}/${itemId}`)
+export async function fetchOwneableItem<T>(entityKey: string, itemId: string, options?: RequestInit): Promise<T> {
+  const res = await fetchWithAuth<{ item: T }>(`/${entityKey}/${itemId}`, options)
   return res.item
 }
 
-export async function createOwnableItem<CreateDto, T>(entityKey: string, data: CreateDto): Promise<T> {
+export async function createOwnableItem<CreateDto, T>(
+  entityKey: string,
+  data: CreateDto,
+  options?: RequestInit,
+): Promise<T> {
   const res = await fetchWithAuth<{ item: T }>(`/${entityKey}`, {
     method: 'POST',
+    body: JSON.stringify(data),
+    ...(options ?? {}),
     headers: {
       'Content-Type': 'application/json',
+      ...(options?.headers ?? {}),
     },
-    body: JSON.stringify(data),
   })
   return res.item
 }
 
-export async function updateOwnableItem<UpdateDto, T>(entityKey: string, itemId: string, data: UpdateDto): Promise<T> {
+export async function updateOwnableItem<UpdateDto, T>(
+  entityKey: string,
+  itemId: string,
+  data: UpdateDto,
+  options?: RequestInit,
+): Promise<T> {
   const res = await fetchWithAuth<{ item: T }>(`/${entityKey}/${itemId}`, {
     method: 'PATCH',
+    body: JSON.stringify(data),
+    ...(options ?? {}),
     headers: {
       'Content-Type': 'application/json',
+      ...(options?.headers ?? {}),
     },
-    body: JSON.stringify(data),
   })
   return res.item
 }
 
-export async function deleteOwnableItem(entityKey: string, itemId: string): Promise<void> {
+export async function deleteOwnableItem(entityKey: string, itemId: string, options?: RequestInit): Promise<void> {
   const res = await fetchWithAuth<void>(`/${entityKey}/${itemId}`, {
     method: 'DELETE',
+    ...(options ?? {}),
   })
   return res
 }
