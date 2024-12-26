c023eae22d2ed6f5ffd217a11c69814f48ef6620
marian2js <marianomdq1@gmail.com>
2024-12-26 19:11:11 -0300

    feat: add fetch current user helper

--- packages/nextjs/src/services/user.service.ts
+++ packages/nextjs/src/services/user.service.ts
@@ -3,6 +3,15 @@
 import { BaseUser } from '../types'
 import { fetchWithAuth } from '../utils/fetch.utils'
 
+export async function fetchMe<U extends BaseUser | null>() {
+  try {
+    const data = await fetchWithAuth<{ user: U }>(`/users/me`)
+    return data?.user ?? null
+  } catch {
+    return null
+  }
+}
+
 export async function fetchAuthUser<U extends BaseUser>() {
   const data = await fetchWithAuth<{ user: U }>(`/users/me`)
   return data.user
